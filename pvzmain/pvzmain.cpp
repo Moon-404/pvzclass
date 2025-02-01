5747960d2ef5ed18946d261199a15489b4f0ab06
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-02-01 20:36:00 +0800

    revert macro & r32

--- pvzmain/pvzmain.cpp
+++ pvzmain/pvzmain.cpp
@@ -1,6 +1,20 @@
 #include "../pvzclass/pvzclass.h"
+#include "../pvzclass/Events/Events.h"
 
 int main()
 {
-	return(0);
+	PVZ::Memory::localExecute = false;
+	DWORD pid = ProcessOpener::Open();
+	if (!pid) return 1;
+	PVZ::InitPVZ(pid);
+	while (PVZ::GetPVZApp()->GameState != PVZGameState::MainMenu) Sleep(10);
+	PVZ::Memory::immediateExecute = true;
+	PVZ::Memory::InjectDll("pvzdll.dll");
+	PVZ::Memory::InvokeDllProc("init");
+	EnableBackgroundRunning();
+	DisableInitialLawnmover();
+	DisableIceLevelFailSound();
+	DrawPlantReanimEvent();
+	PVZ::QuitPVZ();
+	return 0;
 }
