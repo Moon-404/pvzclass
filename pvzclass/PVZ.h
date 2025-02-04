1de7a0ddc353bc3481dbea0c3599f2e33f7cc501
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2025-02-04 17:06:02 +0800

    Board::GetAllLawnmovers() obey standard

--- pvzclass/PVZ.h
+++ pvzclass/PVZ.h
@@ -379,7 +379,7 @@ namespace PVZ
 		std::vector<Plant> GetAllPlants();
 		std::vector<Projectile> GetAllProjectile();
 		std::vector<Coin> GetAllCoins();
-		std::vector<SPT<Lawnmover>> GetAllLawnmovers();
+		std::vector<Lawnmover> GetAllLawnmovers();
 		std::vector<SPT<Griditem>> GetAllGriditems();
 		SPT<Lawn> GetLawn();
 		SPT<Icetrace> GetIcetrace();
