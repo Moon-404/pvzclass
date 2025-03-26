99d9a0aa803bcf97f53df6b41751dc9479a18c4c
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-03-26 19:28:26 +0800

    Update PlantTakeDamageEvent.hpp

--- pvzclass/Events/PlantTakeDamageEvent.hpp
+++ pvzclass/Events/PlantTakeDamageEvent.hpp
@@ -1,7 +1,46 @@
 ﻿#pragma once
 #include "DLLEvent.h"
 
-// 植物受到僵尸豌豆伤害事件。
+// 植物受到啃食伤害事件。
+// 此事件不考虑 IZ 模式下的双倍伤害。若要手动避免，请另行结算后取消伤害，
+// @param 依次为：植物地址、僵尸地址、GameObjectType::OBJECT_TYPE_NONE、伤害数值（非引用）
+// @return 调整后的伤害数值，负数会取消伤害，
+class PlantTakeEatDamageEvent : public DLLEvent
+{
+public:
+	PlantTakeEatDamageEvent();
+	PlantTakeEatDamageEvent(const char* name);
+};
+
+PlantTakeEatDamageEvent::PlantTakeEatDamageEvent()
+{
+	PlantTakeEatDamageEvent::PlantTakeEatDamageEvent("onPlantTakeEatDamage");
+}
+
+PlantTakeEatDamageEvent::PlantTakeEatDamageEvent(const char* name)
+{
+	DWORD procAddress = PVZ::Memory::GetProcAddress(name);
+	hookAddress = 0x52FCF0;
+	rawlen = 7;
+	BYTE code[] =
+	{
+		PUSH(4),
+		PUSHDWORD(GameObjectType::OBJECT_TYPE_NONE),
+		PUSH_EBP,
+		PUSH_ESI,
+		INVOKE(procAddress),
+		ADD_ESP(16),
+		TEST_EUX_EVX(REG_EAX, REG_EAX),
+		JNS(8),
+
+		POPAD,
+		MOV_ECX(0x52FD2E),
+		JMP_REG32(REG_ECX)
+	};
+	start(STRING(code));
+}
+
+// 植物受到子弹伤害事件。
 // @param 依次为：植物地址、子弹地址、子弹的 GameObjectType、伤害数值（非引用）
 // @return 调整后的伤害数值，负数会取消伤害，
 class PlantTakeProjectileDamageEvent : public DLLEvent
@@ -13,7 +52,7 @@ public:
 
 PlantTakeProjectileDamageEvent::PlantTakeProjectileDamageEvent()
 {
-	PlantTakeProjectileDamageEvent::PlantTakeProjectileDamageEvent("onPlantTakePeaDamage");
+	PlantTakeProjectileDamageEvent::PlantTakeProjectileDamageEvent("onPlantTakeProjectileDamage");
 }
 
 PlantTakeProjectileDamageEvent::PlantTakeProjectileDamageEvent(const char* name)
