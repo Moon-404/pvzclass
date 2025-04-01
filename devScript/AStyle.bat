9203ec713cdb7a5115ea86fd9495c63fd0273978
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2025-04-01 09:52:47 +0800

    move astyle params to /devTools

--- devScript/AStyle.bat
+++ devScript/AStyle.bat
@@ -1,1 +1,2 @@
+cd ../
 for /r %%f in (*.cpp;*.h;*.hpp) do astyle.exe -n -A1 --indent=force-tab=4 --indent-after-parens --indent-namespaces --indent-col1-comments --pad-comma --pad-include --squeeze-lines=1 --squeeze-ws %%f
