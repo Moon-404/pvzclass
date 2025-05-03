8baff666fbb91d6a59cce97b259db70ad8605158
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-05-03 16:54:32 +0800

    ProcessOpener doc

--- pvzclass/ProcessOpener.h
+++ pvzclass/ProcessOpener.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include <windows.h>
+/// @brief 用于加载 PVZ 主程序的类。
 class ProcessOpener
 {
 public:
@@ -7,9 +8,21 @@ public:
 	static LPCWSTR WindowTitle;
 	static LPCWSTR Directory;
 	static LPCWSTR ExecuteableName;
+	/// @brief 根据进程名称寻找 PVZ 进程
+	/// @param processName 进程名称
+	/// @return 找到的 PVZ 进程 ID
 	static DWORD OpenByProcessName(LPCWSTR processName);
+	/// @brief 根据窗口标题寻找 PVZ 进程
+	/// @param processName 窗口名称
+	/// @return 找到的 PVZ 进程 ID
 	static DWORD OpenByWindowTitle(LPCWSTR processName);
+	/// @brief 根据给定的目录和可执行文件名称，打开文件并获得其进程 ID
+	/// @param directory 文件所在文件夹目录
+	/// @param executeableName 可执行文件名称
+	/// @return 打开的 PVZ 进程 ID
 	static DWORD OpenByFilePath(LPCWSTR directory, LPCWSTR executeableName);
+	/// @brief 使用默认设置，尝试寻找 PVZ 进程
+	/// @return 找到的 PVZ 进程 ID
 	static DWORD Open();
 };
 
