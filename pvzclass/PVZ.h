d21c42d992b6fd1647c4429d2fe7e6c4370d5c44
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-05-09 21:19:39 +0800

    Griditem derivate doc

--- pvzclass/PVZ.h
+++ pvzclass/PVZ.h
@@ -825,6 +825,7 @@ namespace PVZ
 		/// @brief 移除此物件
 		void Remove();
 	};
+	/// @brief 墓碑
 	class Grave :public PVZ::Griditem
 	{
 	public:
@@ -832,77 +833,110 @@ namespace PVZ
 		Grave(int indexoraddress) :Griditem(indexoraddress) {};
 		INT_PROPERTY(AppearedValue, __get_AppearedValue, __set_AppearedValue, 0x18);
 	};
+	/// @brief 弹坑
 	class Crater :public PVZ::Griditem
 	{
 	public:
 		static const GriditemType::GriditemType ItemType = GriditemType::Crater;
 		Crater(int indexoraddress) :Griditem(indexoraddress) {};
+		/// @brief 消失倒计时
 		INT_PROPERTY(DisappearCountdown, __get_DisappearCountdown, __set_DisappearCountdown, 0x18);
 	};
+	/// @brief 水族馆模式的脑子
 	class AquariumBrain :public PVZ::Griditem
 	{
 	public:
 		static const GriditemType::GriditemType ItemType = GriditemType::AquariumBrain;
 		AquariumBrain(int indexoraddress) :Griditem(indexoraddress) {};
+		/// @brief 实际的 X 坐标
 		T_PROPERTY(FLOAT, X, __get_X, __set_X, 0x24);
+		/// @brief 实际的 Y 坐标
 		T_PROPERTY(FLOAT, Y, __get_Y, __set_Y, 0x28);
 	};
+	/// @brief 禅境花园的蜗牛
 	class Snail :public PVZ::Griditem
 	{
 	public:
 		static const GriditemType::GriditemType ItemType = GriditemType::Snail;
 		Snail(int indexoraddress) :Griditem(indexoraddress) {};
+		/// @brief 实际的 X 坐标
 		T_PROPERTY(FLOAT, X, __get_X, __set_X, 0x24);
+		/// @brief 实际的 Y 坐标
 		T_PROPERTY(FLOAT, Y, __get_Y, __set_Y, 0x28);
+		/// @brief 目标 X 坐标
 		T_PROPERTY(FLOAT, TargetX, __get_TargetX, __set_TargetX, 0x2C);
+		/// @brief 目标 Y 坐标
 		T_PROPERTY(FLOAT, TargetY, __get_TargetY, __set_TargetY, 0x30);
 	};
+	/// @brief 罐子
 	class Vase :public PVZ::Griditem
 	{
 	public:
 		static const GriditemType::GriditemType ItemType = GriditemType::Vase;
 		Vase(int indexoraddress) :Griditem(indexoraddress) {};
+		/// @brief 罐子外观。本质是场地物件的状态。
 		T_PROPERTY(VaseSkin::VaseSkin, Skin, __get_Skin, __set_Skin, 0xC);
+		/// @brief 内含的僵尸类型
 		T_PROPERTY(ZombieType::ZombieType, ContentZombie, __get_ContentZombie, __set_ContentZombie, 0x3C);
+		/// @brief 内含的种子卡片类型
 		T_PROPERTY(SeedType::SeedType, ContentPlant, __get_ContentPlant, __set_ContentPlant, 0x40);
+		/// @brief 罐子实际含有的物品类型
 		T_PROPERTY(VaseContent::VaseContent, Content, __get_Content, __set_Content, 0x44);
+		/// @brief 是否高亮
 		T_READONLY_PROPERTY(BOOLEAN, MouseEnter, __get_MouseEnter, 0x48);
+		/// @brief 透明状态倒计时
 		INT_PROPERTY(TransparentCountDown, __get_TransparentCountDown, __set_TransparentCountDown, 0x4C);
+		/// @brief 储存阳光数
 		INT_PROPERTY(Sun, __get_Sun, __set_Sun, 0x50);
+		/// @brief 直接开启此罐子
 		void Open();
 	};
+	/// @brief IZ 模式的脑子
 	class IZBrain :public PVZ::Griditem
 	{
 	public:
 		static const GriditemType::GriditemType ItemType = GriditemType::IZBrain;
 		IZBrain(int indexoraddress) :Griditem(indexoraddress) {};
+		/// @brief 脑子剩余生命值
 		INT_PROPERTY(Hp, __get_Hp, __set_Hp, 0x18);
+		/// @brief X 坐标
 		T_PROPERTY(FLOAT, X, __get_X, __set_X, 0x24);
+		/// @brief Y 坐标
 		T_PROPERTY(FLOAT, Y, __get_Y, __set_Y, 0x28);
 	};
-	// 在非传送门关卡中只有画面效果不会触发传送
-	// 提供几个方法用于模拟传送门关卡中的传送效果
+	/// @brief 传送门
+	/// @note 非传送门关卡，传送门只有画面效果，不会触发传送，\n
+	///		因此此类只提供几个方法用于模拟传送门关卡中的传送效果
 	class Portal :public PVZ::Griditem
 	{
 	public:
 		Portal(int indexoraddress) :Griditem(indexoraddress) {};
+		/// @brief 关闭此传送门
 		void Close();
-		// 判定僵尸是否进入这个传送门
+		/// @brief 判定僵尸是否进入这个传送门
+		/// @param zombie 被判定的僵尸
+		/// @return 僵尸是否进入传送门
+		/// @todo 实现一个参数为 PVZ::Zombie 的版本
 		bool isZombieIn(std::shared_ptr<PVZ::Zombie> zombie);
-		// 获取僵尸从这个传送门出来时的X坐标
+		/// @brief 获取僵尸从这个传送门出来时的X坐标
+		/// @return 僵尸从这个传送门出来时的X坐标
 		int getZombieOutX();
-		// 判定子弹是否进入这个传送门
+		/// @brief 判定子弹是否进入这个传送门
+		/// @param projectile 被判定的子弹
+		/// @return 子弹是否进入这个传送门
 		bool isProjectileIn(std::shared_ptr<PVZ::Projectile> projectile);
-		// 获取子弹从这个传送门射出时的X坐标
+		/// @brief 获取子弹从这个传送门射出时的X坐标
+		/// @param 子弹从这个传送门射出时的X坐标
 		int getProjectileOutX();
 	};
+	/// @brief 椭圆形传送门，颜色为蓝框冒白光
 	class CirclePortal : public Portal
 	{
 	public:
 		static const GriditemType::GriditemType ItemType = GriditemType::PortalBlue;
 		CirclePortal(int indexoraddress) : Portal(indexoraddress) {};
 	};
-	/// @brief 矩形传送门。
+	/// @brief 矩形传送门，颜色为黄框
 	class SquarePortal : public Portal
 	{
 	public:
