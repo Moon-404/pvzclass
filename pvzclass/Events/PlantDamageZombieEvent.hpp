fa441c11123798b3b1e96a0cee0418f9162876ec
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2025-05-13 00:47:28 +0800

    Update PlantDamageZombieEvent.hpp

--- pvzclass/Events/PlantDamageZombieEvent.hpp
+++ pvzclass/Events/PlantDamageZombieEvent.hpp
@@ -8,6 +8,7 @@ namespace PVZEvent
 		PLANTDAMAGETYPE_RETALIATE,
 		PLANTDAMAGETYPE_BOWLING,
 		PLANTDAMAGETYPE_CHOMPER,
+		PLANTDAMAGETYPE_AOE,
 	};
 
 	class PlantDamageZombieEvent
@@ -165,9 +166,105 @@ namespace PVZEvent
 			}
 			Part3(const char* str) : Part3(PVZ::Memory::GetProcAddress(str)) {}
 		};
+		class Part4 : public DLLEvent
+		{
+		public:
+			Part4(int address)
+			{
+				hookAddress = 0x4607B2;
+				rawlen = 5;
+				int tmp = PVZ::Memory::Variable;
+				BYTE code[] =
+				{
+					PUSH_EAX,
+					PUSH(0x14),
+					INVOKE(0x61C130),
+					MOV_EUX_EVX(REG_EBP, REG_EAX),
+					POP_EUX(REG_EAX),
+
+					MOV_PTR_EUX_ADD_V_EVX(REG_EBP, REG_ESI, 0),
+					MOV_PTR_EUX_ADD_V_EVX(REG_EBP, REG_EDI, 4),
+					MOV_PTR_EUX_ADD_V_EVX(REG_EBP, REG_EAX, 8),
+					MOV_REG32_PTR_ESP_ADD_V(REG_EAX, 0x20),
+					MOV_PTR_EUX_ADD_V_EVX(REG_EBP, REG_EAX, 0x0C),
+					MOV_PTR_EUX_ADD_V(REG_EBP, 0x10, PLANTDAMAGETYPE_AOE),
+
+					PUSH_EBP,
+					INVOKE(address),
+					ADD_ESP(4),
+					MOV_PTR_ADDR_EUX(REG_EBP, tmp),
+					POPAD,
+					PUSH_EBP,
+					MOV_EUX_PTR_ADDR(REG_EBP, tmp),
+
+					MOV_EUX_PTR_EVX_ADD_V(REG_EAX, REG_EBP, 0x0C),
+					MOV_PTR_ESP_ADD_V_EUX(REG_EAX, 0),
+
+					MOV_EUX_PTR_EVX_ADD_V(REG_ESI, REG_EBP, 0),
+					MOV_EUX_PTR_EVX_ADD_V(REG_EDI, REG_EBP, 4),
+					MOV_EUX_PTR_EVX_ADD_V(REG_EAX, REG_EBP, 8),
+
+					POP_EUX(REG_EBP),
+					PUSH_PTR(tmp),
+					INVOKE(0x61C19A),
+					JMP(1)
+				};
+				start(STRING(code));
+			}
+			Part4(const char* str) : Part4(PVZ::Memory::GetProcAddress(str)) {}
+		};
+		class Part5 : public DLLEvent
+		{
+		public:
+			Part5(int address)
+			{
+				hookAddress = 0x45EE22;
+				rawlen = 5;
+				int tmp = PVZ::Memory::Variable;
+				BYTE code[] =
+				{
+					PUSH_EAX,
+					PUSH(0x14),
+					INVOKE(0x61C130),
+					MOV_EUX_EVX(REG_EDX, REG_EAX),
+					POP_EUX(REG_EAX),
+
+					MOV_PTR_EUX_ADD_V_EVX(REG_EDX, REG_ESI, 0),
+					MOV_PTR_EUX_ADD_V_EVX(REG_EDX, REG_EDI, 4),
+					MOV_PTR_EUX_ADD_V_EVX(REG_EDX, REG_EBP, 8),
+					MOV_REG32_PTR_ESP_ADD_V(REG_EAX, 0x20),
+					MOV_PTR_EUX_ADD_V_EVX(REG_EDX, REG_EAX, 0x0C),
+					MOV_PTR_EUX_ADD_V(REG_EDX, 0x10, PLANTDAMAGETYPE_AOE),
+
+					PUSH_EDX,
+					INVOKE(address),
+					ADD_ESP(4),
+					MOV_PTR_ADDR_EUX(REG_EDX, tmp),
+					POPAD,
+					PUSH_EDX,
+					MOV_EUX_PTR_ADDR(REG_EDX, tmp),
+
+					MOV_EUX_PTR_EVX_ADD_V(REG_EAX, REG_EDX, 0x0C),
+					MOV_PTR_ESP_ADD_V_EUX(REG_EAX, 0),
+
+					MOV_EUX_PTR_EVX_ADD_V(REG_ESI, REG_EDX, 0),
+					MOV_EUX_PTR_EVX_ADD_V(REG_EDI, REG_EDX, 4),
+					MOV_EUX_PTR_EVX_ADD_V(REG_EBP, REG_EDX, 8),
+
+					POP_EUX(PUSH_EDX),
+					PUSH_PTR(tmp),
+					INVOKE(0x61C19A),
+					JMP(1)
+				};
+				start(STRING(code));
+			}
+			Part5(const char* str) : Part5(PVZ::Memory::GetProcAddress(str)) {}
+		};
 		Part1* spikerock1;
 		Part2* bowling;
 		Part3* chomper;
+		Part4* squash;
+		Part5* rowarea;
 	public:
 		PlantDamageZombieEvent()
 		{
@@ -175,18 +272,23 @@ namespace PVZEvent
 			spikerock1 = new Part1(str);
 			bowling = new Part2(str);
 			chomper = new Part3(str);
+			squash = new Part4(str);
+			rowarea = new Part5(str);
 		}
 		PlantDamageZombieEvent(int address)
 		{
 			spikerock1 = new Part1(address);
 			bowling = new Part2(address);
 			chomper = new Part3(address);
+			squash = new Part4(address);
+			rowarea = new Part5(address);
 		}
 		void end()
 		{
 			spikerock1->end();
 			bowling->end();
 			chomper->end();
+			squash->end();
 		}
 	};
 }
