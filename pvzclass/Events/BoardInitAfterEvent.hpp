97ed2b6d49cadb9596f43acd3010a6cb1e626302
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-06-04 20:46:26 +0800

    BoardInitAfterEvent

---
+++ pvzclass/Events/BoardInitAfterEvent.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "DLLEvent.h"
+
+namespace PVZEvent
+{
+	/// @brief Board 初始化完毕事件。
+	/// @param 初始化的 Board 对象
+	class BoardInitAfterEvent : public DLLEventTemplate<0x408668, 6, REG_EAX>
+	{
+	public:
+		BoardInitAfterEvent(const char* str) : DLLEventTemplate() { Init(str); };
+		BoardInitAfterEvent(int address) : DLLEventTemplate() { Init(address); };
+		BoardInitAfterEvent() : DLLEventTemplate() { Init("onBoardInitAfter"); };
+	};
+}
