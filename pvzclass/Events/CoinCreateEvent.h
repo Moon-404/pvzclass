9320e02f333a61735901064f4eed729d70488b30
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2025-07-08 12:43:07 +0800

    CoinCreateEvent doc & new constructor

--- pvzclass/Events/CoinCreateEvent.h
+++ pvzclass/Events/CoinCreateEvent.h
@@ -1,20 +1,12 @@
 #pragma once
 #include "DLLEvent.h"
 
-// Coin创建事件
-// 参数：触发事件的Coin
-// 无返回值
-class CoinCreateEvent : public DLLEvent
+/// @brief Coin鍒涘缓浜嬩欢
+/// @param 瑙﹀彂浜嬩欢鐨凜oin
+class CoinCreateEvent : public DLLEventTemplate<0x40CCCE, 8, REG_EAX>
 {
 public:
-	CoinCreateEvent();
+	CoinCreateEvent() : CoinCreateEvent("onCoinCreate") {};
+	CoinCreateEvent(const char* str) : DLLEventTemplate() { Init(str); };
+	CoinCreateEvent(int address) : DLLEventTemplate() { Init(address); };
 };
-
-CoinCreateEvent::CoinCreateEvent()
-{
-	int procAddress = PVZ::Memory::GetProcAddress("onCoinCreate");
-	hookAddress = 0x40CCCE;
-	rawlen = 8;
-	BYTE code[] = { PUSH_EAX, INVOKE(procAddress), ADD_ESP(4) };
-	start(STRING(code));
-}
