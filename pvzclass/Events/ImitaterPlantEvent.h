156810782285f18f5f18e71534ed19d4a443d561
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-07-09 10:41:36 +0800

    ImitaterPlantEvent doc & new constructor

--- pvzclass/Events/ImitaterPlantEvent.h
+++ pvzclass/Events/ImitaterPlantEvent.h
@@ -1,21 +1,13 @@
-﻿#pragma once
+#pragma once
 #include "DLLEvent.h"
 
-// 模仿者即将生成新植物的事件。
+/// @brief 模仿者即将生成新植物的事件。
 /// @param 触发事件的模仿者（注意：不是即将生成的新植物）。
 /// @return 是否继续结算该事件。
-class ImitaterPlantEvent : public DLLEvent
+class ImitaterPlantEvent : public DLLEventTemplate<0x466B89, 6, REG_ESI>
 {
 public:
-	ImitaterPlantEvent();
+	ImitaterPlantEvent(const char* str) : DLLEventTemplate() { Init(str); };
+	ImitaterPlantEvent(int address) : DLLEventTemplate() { Init(address); };
+	ImitaterPlantEvent() : DLLEventTemplate() { Init("onLawnmowerUpdate"); };
 };
-
-ImitaterPlantEvent::ImitaterPlantEvent()
-{
-	int procAddress = PVZ::Memory::GetProcAddress("onImitaterPlant");
-	hookAddress = 0x466B89;
-	rawlen = 6;
-	BYTE code[] = { PUSH_ESI, INVOKE(procAddress), ADD_ESP(4) };
-	start(STRING(code));
-}
-
