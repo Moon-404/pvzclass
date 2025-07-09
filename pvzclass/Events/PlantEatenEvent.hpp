21d491d0a12752965f2212d40becaa2c09df2f26
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-07-09 11:00:19 +0800

    PlantEatenEvent doc & new constructor

--- pvzclass/Events/PlantEatenEvent.hpp
+++ pvzclass/Events/PlantEatenEvent.hpp
@@ -1,11 +1,13 @@
-﻿#pragma once
+#pragma once
 #include "DLLEvent.h"
 
-// 植物被啃死事件。
+/// @brief 植物被啃死事件。
 /// @param 依次为：触发事件的植物、啃食该植物的僵尸。
 /// @return 是否继续结算原版的结算过程。
 class PlantEatenEvent : public BoolDLLEventTemplate<0x52FD3D, 5, 0x52FDEE, REG_EBP, REG_ESI>
 {
 public:
+	PlantEatenEvent(const char* str) : BoolDLLEventTemplate() { Init(str); };
+	PlantEatenEvent(int address) : BoolDLLEventTemplate() { Init(address); };
 	PlantEatenEvent() : BoolDLLEventTemplate() { Init("onPlantEaten"); };
 };
