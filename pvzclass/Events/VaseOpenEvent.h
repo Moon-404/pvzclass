c8e5a83f92dc266b0c165a61b30f7280cbf1d6bc
Violet Nonbloosom <violetnonbloosom@yeah.net>
2025-07-22 12:04:19 +0800

    VaseOpenEvent doc & new constructor

--- pvzclass/Events/VaseOpenEvent.h
+++ pvzclass/Events/VaseOpenEvent.h
@@ -1,31 +1,14 @@
-﻿#pragma once
+#pragma once
 #include "DLLEvent.h"
 
-// 开罐事件。
-// 时机上优先级高于开罐的其他操作。
+/// @brief 罐子被开启事件。
+/// @note 时机上优先级高于开罐的其他操作。
 /// @param 依次为：触发事件的 Challenge、触发该事件的罐子基址。
 /// @return 是否继续结算罐子被砸开的事件。返回“否”会阻止罐子被敲开。
-class VaseOpenEvent : public DLLEvent
+class VaseOpenEvent : public BoolDLLEventTemplate<0x429AC0, 6, 0x429DA1, MEM_ESP_ADD(0x28), MEM_ESP_ADD(0x28)>
 {
 public:
-	VaseOpenEvent();
+	VaseOpenEvent() : VaseOpenEvent("onVaseOpen") {};
+	VaseOpenEvent(const char* str) : BoolDLLEventTemplate() { Init(str); };
+	VaseOpenEvent(int address) : BoolDLLEventTemplate() { Init(address); };
 };
-
-VaseOpenEvent::VaseOpenEvent()
-{
-	int procAddress = PVZ::Memory::GetProcAddress("onVaseOpen");
-	hookAddress = 0x429AC0;
-	rawlen = 6;
-	BYTE code[] =
-	{
-		PUSH_PTR_ESP_ADD_V(0x28),
-		PUSH_PTR_ESP_ADD_V(0x28),
-		INVOKE(procAddress),
-		ADD_ESP(8),
-
-		TEST_AL_AL,
-		JNZ(3),
-		RETN(8)
-	};
-	start(STRING(code));
-}
