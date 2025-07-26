704c6e41c50512d928492459e345e11f39e3f3e7
Violet Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2025-07-26 22:02:28 +0800

    PlantShoveledEvent bug fix

--- pvzclass/Events/Events.h
+++ pvzclass/Events/Events.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "BoardUpdateGameEvent.hpp"
 #include "CoinCollectEvent.h"
 #include "CoinCreateEvent.h"
@@ -25,7 +25,6 @@
 #include "ProjectileHitZombieEvent.h"
 #include "ProjectileRemoveEvent.h"
 #include "PuzzlePhaseCompleteEvent.hpp"
-#include "SeedCardClickEvent.h"
 #include "UpdateAppEvent.h"
 #include "UpdateGameObjectsEvent.h"
 #include "ZombieBlastEvent.h"
