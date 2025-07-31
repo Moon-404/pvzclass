c2a16509aa52262af4524d75becc4e11d76325e5
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-07-31 18:06:43 +0800

    ZombieEatSoundEvent bug fix

--- pvzclass/Events/ZombieEatSoundEvent.hpp
+++ pvzclass/Events/ZombieEatSoundEvent.hpp
@@ -5,7 +5,7 @@
 /// @note 魅惑菇和大蒜在此时机发挥作用。
 /// @param 依次为：触发事件的僵尸、被其啃食的植物。
 /// @return 是否执行原版对该事件的结算。
-class ZombieEatSoundEvent : public DLLEventTemplate<0x52B964, 6, 0x52BAD7, REG_ESI, REG_EDI, >
+class ZombieEatSoundEvent : public DLLEventTemplate<0x52B964, 6, 0x52BAD7, REG_ESI, REG_EDI>
 {
 public:
 	ZombieEatSoundEvent() : DLLEventTemplate() { Init("onZombieEatSound"); };
