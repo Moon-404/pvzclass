a54d0dd97f92c17aee28495420e99d16fe40a979
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-17 18:35:21 +0800

    feat(Event): MagnetShroomFindTargetEvents

--- pvzclass/Events/PlantFindTargetEvents.hpp
+++ pvzclass/Events/PlantFindTargetEvents.hpp
@@ -38,4 +38,27 @@ namespace PVZEvent
 		StarfruitFindTargetEvent(int address) : ThreeStateEventTemplate() { Init(address); };
 		StarfruitFindTargetEvent() : StarfruitFindTargetEvent("onStarFruitFindTarget") {};
 	};
+
+	/// @brief 磁力菇索敌僵尸事件
+	/// @note 该事件只重载行差判定和类型判定，不影响基础类型判定和范围判定
+	/// @param 触发事件的植物，植物判定的僵尸
+	/// @return 若为负数，则按原版处理；若为 0，则为不可选中；若为正数，则为强制可选。
+	class MagnetShroomFindTargetRTEvent : public ThreeStateEventTemplate<0x46209C, 6, 0x4620F4, 0x46218F, REG_EBX, REG_EDI>
+	{
+	public:
+		MagnetShroomFindTargetRTEvent(const char* str) : ThreeStateEventTemplate() { Init(str); };
+		MagnetShroomFindTargetRTEvent(int address) : ThreeStateEventTemplate() { Init(address); };
+		MagnetShroomFindTargetRTEvent() : MagnetShroomFindTargetRTEvent("onMagnetShroomFindTargetRT") {};
+	};
+
+	/// @brief 磁力菇索敌场地物品事件
+	/// @param 触发事件的植物，植物判定的场地物品
+	/// @return 若为负数，则按原版处理；若为 0，则为不可选中；若为正数，则为强制可选。
+	class MagnetShroomFindGriditemEvent : public ThreeStateEventTemplate<0x4621E8, 8, 0x46224F, 0x462257, MEM_ESP_ADD(0x34), REG_EBX>
+	{
+	public:
+		MagnetShroomFindGriditemEvent(const char* str) : ThreeStateEventTemplate() { Init(str); };
+		MagnetShroomFindGriditemEvent(int address) : ThreeStateEventTemplate() { Init(address); };
+		MagnetShroomFindGriditemEvent() : MagnetShroomFindGriditemEvent("onMagnetShroomFindGriditem") {};
+	};
 }
