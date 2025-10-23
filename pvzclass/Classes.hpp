432c126110319c472cc5bca9cdb56f6c0dd5a549
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-10-23 11:01:29 +0800

    feat: Add new hpp to Classes.hpp

--- pvzclass/Classes.hpp
+++ pvzclass/Classes.hpp
@@ -3,6 +3,9 @@
 #include "Classes/SaveData.hpp"
 #include "Classes/ZenGarden.hpp"
 
+#include "Classes/SeedBank.hpp"
+#include "Classes/Challenge.hpp"
+
 #include "Classes/Definition.hpp"
 
 #include "Classes/ChallengeScreen.hpp"
