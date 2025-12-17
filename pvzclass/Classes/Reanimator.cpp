bc9a7c3ceaa075341af4baef7eb7a2849fd54c61
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-12-17 17:45:17 +0800

    fix(Reanimator): Reposition save&load bug fix

--- pvzclass/Classes/Reanimator.cpp
+++ pvzclass/Classes/Reanimator.cpp
@@ -14,4 +14,7 @@ void PVZ::ReanimationParams::Reposition(DWORD baseaddress, DWORD num)
 	PVZ::Memory::WriteMemory<int>(0x47377E, num);
 	PVZ::Memory::WriteMemory<DWORD>(0x4737A5, baseaddress);
 	PVZ::Memory::WriteMemory<DWORD>(0x4737F5, baseaddress + 4);
+
+	PVZ::Memory::WriteMemory<int>(0x4814F3, num);
+	PVZ::Memory::WriteMemory<int>(0x48153C, num);
 }
