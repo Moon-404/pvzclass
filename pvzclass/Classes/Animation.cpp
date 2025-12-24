ba7362574304c86016f5ad597cdfdc7e86c673e9
quminzhennan <192948673@qq.com>
2025-12-24 21:18:44 +0800

    fix(Animation):could not get return value

--- pvzclass/Classes/Animation.cpp
+++ pvzclass/Classes/Animation.cpp
@@ -124,11 +124,12 @@ void PVZ::Animation::Play(const char* trackName, int blendType, PVZEnum::ReanimL
 	PVZ::Memory::Execute(STRING(__asm__Reanimation__Play));
 }
 
-byte __asm__Reanimation__IsAnimPlaying[24]
+byte __asm__Reanimation__IsAnimPlaying[29]
 {
 	MOV_EDX(0),
 	MOV_ESI(0),
 	INVOKE(0x4745B0),
+	MOV_PTR_ADDR_EAX(0),
 	RET
 };
 
@@ -137,6 +138,7 @@ bool PVZ::Animation::IsAnimPlaying(const char* trackName)
 	PVZ::Memory::WriteArray<const char>(PVZ::Memory::Variable + 100, trackName, std::strlen(trackName) + 1);
 	SETARG(__asm__Reanimation__IsAnimPlaying, 1) = PVZ::Memory::Variable + 100;
 	SETARG(__asm__Reanimation__IsAnimPlaying, 6) = BaseAddress;
+	SETARG(__asm__Reanimation__IsAnimPlaying, 24) = PVZ::Memory::Variable;
 	return PVZ::Memory::Execute(STRING(__asm__Reanimation__IsAnimPlaying));
 }
 
