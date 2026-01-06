aada3645dbbcc45d8c6067b5255db22e110d40b6
Moon-404 <33384538+Moon-404@users.noreply.github.com>
2026-01-06 19:20:02 +0800

    feat(CutScene): PlaceStreetZombie

--- pvzclass/Classes/CutScene.cpp
+++ pvzclass/Classes/CutScene.cpp
@@ -16,3 +16,29 @@ void PVZ::CutScene::PlaceStreetZombie(ZombieType::ZombieType type, int x, int y)
 		.ret()
 	);
 }
+
+void PVZ::CutScene::PlaceStreetZombie(ZombieType::ZombieType type, bool grid[5][5])
+{
+	int address = 0;
+	if (!PVZ::Memory::localExecute)
+	{
+		address = PVZ::Memory::AllocMemory(0, 25);
+		for (int i = 0; i < 5; i++) PVZ::Memory::WriteArray<bool>(address + i * 5, grid[i], sizeof(grid[i]));
+	}
+	else
+	{
+		address = (int)grid;
+	}
+	PVZ::Memory::Execute(AsmBuilder()
+		.push(address)
+		.mov_reg_imm(REG_ESI, type)
+		.mov_reg_imm(REG_EBX, BaseAddress)
+		.invoke(0x439590)
+		.ret()
+	);
+	if (!PVZ::Memory::localExecute)
+	{
+		for (int i = 0; i < 5; i++) PVZ::Memory::ReadArray<bool>(address + i * 5, grid[i], sizeof(grid[i]));
+		PVZ::Memory::FreeMemory(address);
+	}
+}
--- pvzclass/Classes/CutScene.hpp
+++ pvzclass/Classes/CutScene.hpp
@@ -14,5 +14,10 @@ namespace PVZ
 		/// @note x 和 y 的取值应该是 0,1,2,3,4
 		/// @note 在植物僵尸 2 关卡中，创建鸭子救生圈僵尸会转化为创建豌豆射手僵尸。
 		void PlaceStreetZombie(ZombieType::ZombieType type, int x, int y);
+		/// @brief 寻找空位放置街上的僵尸
+		/// @param type 僵尸类型
+		/// @param grid 该格子是否已被占用
+		/// @note grid 数组会被实时修改更新
+		void PlaceStreetZombie(ZombieType::ZombieType type, bool grid[5][5]);
 	};
 }
